96b0c0dcb7d681b3788ea18454cf282c6c896867
David <raymondinf23@gmail.com>
2021-12-21 19:45:38 -0400

    Implement cors anywhere to deploy on my heroku server.

---
+++ index.js
@@ -0,0 +1,15 @@
+const cors_proxy = require('cors-anywhere');
+
+const port = process.env.PORT || 8080;
+
+const proxy_server = cors_proxy.createServer({
+    originWhitelist: [],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen( { port, host: 'localhost' }, () => {
+    console.log( `Running CORS Anywhere on port: ${ port }` );
+});
+
+proxy_server.on('request', (req, res) => {
+    console.log(req.url);
+});
