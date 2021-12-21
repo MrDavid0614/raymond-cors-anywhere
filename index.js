549eb86ceb7f5bfad4968e4b6b94da56d0439fb3
David <raymondinf23@gmail.com>
2021-12-21 19:49:09 -0400

    Delete unnecesary constant.

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const cors_proxy = require('cors-anywhere');
 
 const port = process.env.PORT || 8080;
 
-const proxy_server = cors_proxy.createServer({
+cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen( { port, host: 'localhost' }, () => {
     console.log( `Running CORS Anywhere on port: ${ port }` );
-});
-
-proxy_server.on('request', (req, res) => {
-    console.log(req.url);
 });
