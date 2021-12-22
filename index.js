9ef97ede03d3bf02f855d5b41fceeff922a30015
David <raymondinf23@gmail.com>
2021-12-21 21:23:55 -0400

    Change default host

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cors_proxy = require('cors-anywhere');
 
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
