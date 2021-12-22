c22cc8a290740f8a4a91e94113630c8f31637d44
David <raymondinf23@gmail.com>
2021-12-21 20:08:51 -0400

    Add host constant to index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const cors_proxy = require('cors-anywhere');
 
+const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen( { port, host: 'localhost' }, () => {
+}).listen( { port, host }, () => {
     console.log( `Running CORS Anywhere on port: ${ port }` );
 });
